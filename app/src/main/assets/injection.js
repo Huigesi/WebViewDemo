76bb6a76245b9c3e1c369d9d8a41c24369825540
Huigesi <hui791339970@163.com>
2018-06-26 17:08:24 +0800

    ss

--- app/src/main/assets/injection.js
+++ app/src/main/assets/injection.js
@@ -1,3 +1,4 @@
+/*
 var script = document.createElement('script');
 script.type = 'text/javascript';
 var username = document.getElementById('username');
@@ -5,4 +6,7 @@ username.value='cuiyuc';
 var password=document.getElementById('password');
 password.value='charles1';
 var loginbtn= document.getElementById('btn-submit');
-loginbtn.click();
+loginbtn.click();*/
+var script = document.createElement('script');
+script.type='text/javascript';
+vuehandle.list.unshift({name:'注入',time:new Date()});
